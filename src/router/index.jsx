7efe4b52265302dad51d29bfcd2911ac726bb064
NurdauletShinpolatov <nshinpolatov@gmail.com>
2023-12-29 18:30:32 +0500

    added uncompleted customization

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,32 +1,17 @@
 import { observer } from 'mobx-react-lite';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import AuthLayout from '../layouts/AuthLayout';
 import MainLayout from '../layouts/MainLayout';
-import Login from '../modules/Login';
-import authStore from '../store/auth.store';
-import Dashboard from '../modules/Dashboard';
+import { Home } from '../pages';
+import About from '../pages/about';
 
 const Router = () => {
-	const { isAuth } = authStore;
-
-	if (!isAuth)
-		return (
-			<Routes>
-				<Route path="/" element={<AuthLayout />}>
-					<Route index element={<Navigate to="/login " />} />
-					<Route path="login" element={<Login />} />
-					<Route path="*" element={<Navigate to="/login" />} />
-				</Route>
-				<Route path="*" element={<Navigate to="/login" />} />
-			</Routes>
-		);
 
 	return (
 		<Routes>
 			<Route path="/" element={<MainLayout />}>
-				<Route index element={<Navigate to="/dashboard" />} />
-				<Route path="dashboard" element={<Dashboard />} />
-				<Route path="*" element={<Navigate to="/dashboard" />} />
+				<Route index element={<Home />} />
+				<Route path="home" element={<Home />} />
+				<Route path="about" element={<About />} />
 			</Route>
 
 			<Route path="*" element={<Navigate to="/dashboard" />} />
